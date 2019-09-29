77b83b0bd96f82ce45ce7333dd600b32bb5424d6
Dito <bukiadima@gmail.com>
2019-09-29 22:18:20 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+// console.log(4/0);
+// console.log('string'*9);
+
+// let something;
+// console.log(something);
+
+// let person = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.png'];
+
+// console.log(arr[0]);
+
+// alert("hello world !");
+
+// let answer = prompt("Are you 18 ?", "Yes");
+
+// console.log(typeof(answer));
+// let answer = confirm("Are you here ?");
+
+// console.log(answer);
+
+// let answer = +prompt("Are you 18 ?", "Yes");
+
+// console.log( typeof(answer) ); 
+
+
+// console.log("arr" + " - object");
+// console.log(4 + + " - object");
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+
+    console.log(isChecked || !isClose); 
